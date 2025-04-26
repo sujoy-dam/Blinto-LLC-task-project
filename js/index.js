bf0056a65a7f626963a400cb91bae46d7fd8ec67
sujoy-dam <sujooy01@gmail.com>
2025-04-27 03:52:56 +0600

    design FAQ section done

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ const mobileMenu = document.getElementById('mobile-menu');
 //     console.log('connected')
 // //   mobileMenu.classList.toggle('show');
 // });
-menuIcon.addEventListener('click', ()=>{
-    if(mobileMenu.style.display === 'none'){
+menuIcon.addEventListener('click', () => {
+    if (mobileMenu.style.display === 'none') {
         mobileMenu.style.display = "block"
-    }else{
-        mobileMenu.style.display='none'
+    } else {
+        mobileMenu.style.display = 'none'
     }
 })
 
@@ -25,12 +25,26 @@ menuIcon.addEventListener('click', ()=>{
 const loginButton = document.getElementById('log-in');
 
 loginButton.addEventListener('click', () => {
-  window.location.href = '../html/login.html'; // je page e jete chao tar URL
+    window.location.href = '../html/login.html'; // je page e jete chao tar URL
 });
 
 // signUp file 
 const signupButton = document.getElementById('sign-up');
 
 signupButton.addEventListener('click', () => {
-  window.location.href = '../html/signup.html'; // je page e jete chao tar URL
+    window.location.href = '../html/signup.html'; // je page e jete chao tar URL
+});
+
+// show testimonials
+// const showTestimonials = document.getElementsByClassName('')
+
+document.getElementById('testimonial-show').addEventListener('click', function () {
+    const hiddenCards = document.querySelectorAll('.testimonials-img');
+
+    hiddenCards.forEach(function (card) {
+        card.style.display = 'block'; // show the hidden cards
+        card.classList.remove('hidden'); // optional: remove hidden class
+    });
+
+    this.style.display = 'none'; // button hide after click
 });
